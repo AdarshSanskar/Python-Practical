a3d82418fa39d2bbcae69275917b8ac4e21737e9
AdarshTile <astile1424@gmail.com>
2025-04-21 18:02:34 +0530

    Commit

---
+++ Practical Exam/Pr1.py
@@ -0,0 +1,5 @@
+#Practical No. 1: Write a program to find the area of a triangle
+breadth = float(input("Enter Breadth of Triangle : "))
+height = float(input("Enter Height of Triangle : "))
+area = 0.5 * breadth * height
+print("Area of Triangle : ", area)
