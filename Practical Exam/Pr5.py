773536673a6249ab0fa2978238e54b63711d0fcf
AdarshTile <astile1424@gmail.com>
2025-04-21 19:22:14 +0530

    Commit

--- Practical Exam/Pr5.py
+++ Practical Exam/Pr5.py
@@ -1,6 +1,10 @@
-#Practical No. 5: Write a Program to create user defined to Check if a number is odd or even
-num = int(input("Enter any Number : "))
-if num % 2 == 0:
-    print("The Number is Even")
-else:
-    print("The Number is Odd")
+#Practical No. 5: Write a Program to create user defined Function to Check if a number is odd or even
+
+def check(num):
+    if num % 2 == 0:
+        print("The Number is Even")
+    else:
+        print("The Number is Odd")
+
+no = int(input("Enter any Number : "))
+check(no)
